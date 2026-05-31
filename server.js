3553d165e8027a9e2d8b2db3e97278b528ea7579
omarobaied2016-cmyk <omar.obaied2016@gmail.com>
2026-05-31 09:15:27 +0300

    server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`RiskAtlas landing running on port ${PORT}`);
+});
